cde1e57730ddb05a756e31e6b448015a812f299d
Hasnathasan <hasnatoooooooo@gmail.com>
2023-08-03 10:36:27 +0600

    Question 2 added

--- src/Pages/Editor/Editor.jsx
+++ src/Pages/Editor/Editor.jsx
@@ -1,20 +1,46 @@
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import { Checkbox } from "@material-tailwind/react";
 const Editor = () => {
   const [categories, setCategories] = useState([]);
-  console.log(categories);
+  const [sentence, setSentence] = useState("");
+  const [previewSentence, setPreviewSentence] = useState("")
+  const [selectedWords, setSelectedWords] = useState([]);
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
-    console.log(data);
     const { category1, category2, category3, category4 } = data;
     const newCategories = [category1, category2, category3, category4];
-    console.log(newCategories);
     setCategories(newCategories);
   };
+
+  const handleSelection = () => {
+    const selectedText = window.getSelection().toString().trim();
+    if (selectedText) {
+      setSelectedWords([...selectedWords, selectedText]);
+      setSentence(sentence.replace(selectedText, `<<${selectedText}>>`));
+      for(let i = 0; i<selectedWords; i++){
+        if(i === 0){
+          setPreviewSentence(sentence)
+        }
+        const word = selectedWords[i];
+        setPreviewSentence(previewSentence.replace(`${word}`, "______"))
+      }
+    }
+    console.log(previewSentence);
+  };
+  const handleInputChange = (e) => {
+    const newSentence = e.target.value;
+    setSentence(newSentence);
+    setSelectedWords([]); // Clear selected words when input changes
+  };
+
   return (
     <div>
       <h1 className="text-4xl font-bold">Edit Your Form With EDFORM</h1>
-      <form  className=" max-w-6xl mx-auto border-2 p-6 rounded-lg border-blue-500" onChange={handleSubmit(onSubmit)}>
+      <form
+        className=" max-w-6xl mx-auto border-2 p-6 rounded-lg border-blue-500"
+        onSelect={handleSubmit(onSubmit)}
+      >
         {/* {Categorized Questions} */}
         <div className="mb-20">
           <h3 className="text-xl font-semibold text-blue-700 uppercase mb-3">
@@ -59,47 +85,100 @@ const Editor = () => {
               </div>
             </div>
             <div className="">
-              <h4 className="text-lg mb-2">Item</h4>
+              <h4 className="text-lg mb-2">Item ------------------------------------------ Belongs to</h4>
               <div className=" flex flex-col gap-2">
-                {categories.length === 0 ? <h1 className="text-xl font-bold">Write a Category First</h1> : categories.map((category, index) => {
-                  if (category !== "") {
-                    return (
-                      <div className="flex gap-20 justify-between items-start" key={index}>
-                        <input
-                        {...register(`item${index + 1}`, { required: false })}
-                          className=" outline-none border border-gray-400 p-2 rounded"
-                          type="text"
-                          placeholder={"Item" + (index + 1)}
-                        />
-                        <select {...register(`cateSelected${index + 1}`, { required: false })} className=" flex flex-col gap-2">
-                          <option>Select Category</option>
-                          {categories.map((category, index) => {
-                            if (category !== "") {
-                              return (
-                                <option key={index} value={category}>
-                                  {category}
-                                </option>
-                              );
-                            }
-                          })}
-                        </select>
-                      </div>
-                    );
-                  }
-                })}
+                {categories.length === 0 ? (
+                  <input
+                  className=" outline-none border border-gray-400 p-2 rounded"
+                  type="text"
+                  placeholder="Item1"
+                />
+                ) : (
+                  categories.map((category, index) => {
+                    if (category !== "") {
+                      return (
+                        <div
+                          className="flex gap-20 justify-between items-start"
+                          key={index}
+                        >
+                          <input
+                            {...register(`item${index + 1}`, {
+                              required: false,
+                            })}
+                            className=" outline-none border border-gray-400 p-2 rounded"
+                            type="text"
+                            placeholder={"Item" + (index + 1)}
+                          />
+                          <select
+                            {...register(`cateSelected${index + 1}`, {
+                              required: false,
+                            })}
+                            className=" flex flex-col gap-2 border border-gray-300 p-3 rounded text-gray-600 outline-none"
+                          >
+                            <option>Select Category</option>
+                            {categories.map((category, index) => {
+                              if (category !== "") {
+                                return (
+                                  <option key={index} value={category}>
+                                    {category}
+                                  </option>
+                                );
+                              }
+                            })}
+                          </select>
+                        </div>
+                      );
+                    }
+                  })
+                )}
               </div>
             </div>
           </div>
         </div>
 
-
-
-
         {/* {Cloze Questions} */}
         <div>
-        <h3 className="text-xl font-semibold text-blue-700 uppercase mb-3">
+          <h3 className="text-xl font-semibold text-blue-700 uppercase mb-3">
             Question 2
           </h3>
+          <div>
+            <h4 className="">Preview</h4>
+            <input
+              className=" outline-none border border-gray-400 p-2 rounded"
+              type="text"
+            />
+            <h4 className="mt-5">
+              Sentence -select a word to make it as a fill up word
+            </h4>
+            <textarea
+            cols={50}
+            rows={1}
+              className=" outline-none border border-gray-400 p-2 rounded"
+              type="text"
+              value={sentence}
+              onChange={handleInputChange}
+              onMouseUp={handleSelection}
+            />
+          </div>
+          <ul>
+            {selectedWords.length !== 0 ? selectedWords.map((word, index) => (
+              <li key={index}>
+                <Checkbox defaultChecked id="ripple-off" ripple={false} disabled={true}/>{" "}
+                <input
+                  className=" outline-none border border-gray-400 p-2 rounded"
+                  type="text"
+                  value={word}
+                  readOnly
+                />
+              </li> 
+            )): <><Checkbox defaultChecked id="ripple-off" ripple={false} disabled={true}/>{" "}
+                <input
+                  className=" outline-none border border-gray-400 p-2 rounded"
+                  type="text"
+                  placeholder="Option 1"
+                  readOnly
+                /></>}
+          </ul>
         </div>
       </form>
     </div>
